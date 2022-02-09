f1455c58a807e8cddd0aeab99baee24a4f4cfb00
benjhood830 <benjhood830@SNS-D-130S0H>
2022-02-09 14:26:43 -0500

    Add project files.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textLabelgo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(241, 133);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(297, 85);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "PLAY GAME";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(242, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 60);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3";
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(362, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 60);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(495, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 60);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "1";
+            this.label1.Visible = false;
+            // 
+            // textLabelgo
+            // 
+            this.textLabelgo.AutoSize = true;
+            this.textLabelgo.BackColor = System.Drawing.Color.Transparent;
+            this.textLabelgo.Font = new System.Drawing.Font("Impact", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLabelgo.ForeColor = System.Drawing.Color.White;
+            this.textLabelgo.Location = new System.Drawing.Point(283, 133);
+            this.textLabelgo.Name = "textLabelgo";
+            this.textLabelgo.Size = new System.Drawing.Size(233, 117);
+            this.textLabelgo.TabIndex = 6;
+            this.textLabelgo.Text = "GO!!!";
+            this.textLabelgo.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(792, 396);
+            this.Controls.Add(this.textLabelgo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label textLabelgo;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SoundPlayer player = new SoundPlayer(Properties.Resources.countdown);
+            player.Play();
+            startButton.Visible = false;
+            label3.Visible = true;
+
+            Refresh(); 
+            Thread.Sleep(1000);
+
+            label3.Visible = false;
+            label2.Visible = true;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            label2.Visible = false;
+            label1.Visible = true;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            label1.Visible = false;
+            BackColor = Color.Green;
+            textLabelgo.Visible = true;
+
+        }
+
+    }
+}
